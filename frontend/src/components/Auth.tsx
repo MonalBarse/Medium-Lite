459f5a699991d512adc1f0eb037652359243e199
MonalBarse <monal.barse.cer22@itbhu.ac.in>
2024-07-23 08:27:57 +0530

    build: started with frontend

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+  return (
+    <div className="flex justify-center flex-col h-screen">
+      <div className="font-blod text-4xl">Create an account</div>
+    </div>
+  );
+};
+
+export default Auth;
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,22 @@
+export const Quote = () => {
+  return (
+    <>
+      <div className="h-screen flex justify-center bg-slate-200 flex-col">
+        <div className="flex justify-center">
+          <div className="max-w-lg  ">
+            <div className="font-bold mt-2 text-2xl">
+              "I really love the culture of this social media platform. I feel
+              like I learn something every time just being on here."
+            </div>
+            <div className=" max-w-lg mt-2 text-xl text-slate-700 font-medium">
+              Julies Wingfield
+            </div>
+            <div className="max-w-lg text-sm  text-slate-600 font-medium">
+              Writer | UK
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
